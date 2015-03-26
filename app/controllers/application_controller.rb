1da5887e189dc3b83dec7e1fe32a55b6f76212b4
kanbara <kanbara@bbtower.co.jp>
2015-03-26 19:57:56 +0900

    p118

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAddressRejected < ActionController::ActionControllerError; end
+
   rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, wigh: :rescue403
+  rescue_from IpAddressRefected, with: :rescue403
 
   private
 
@@ -15,6 +20,11 @@ class ApplicationController < ActionController::Base
     else
       'customer'
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
   end
 
   def rescue500(e)
