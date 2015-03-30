f9ea8427aaf3cb7b6daa454c0bf2f4236a43624a
kanbara <kanbara@bbtower.co.jp>
2015-03-30 18:34:57 +0900

    p207 end enshuu

--- spec/routing/hostname_constraints_spec.rb
+++ spec/routing/hostname_constraints_spec.rb
@@ -17,6 +17,14 @@ describe 'ルーティング' do
     )
   end
 
+  example '顧客トップページ' do
+    expect(get: 'http://example.com/mypage').to route_to(
+      host: 'example.com',
+      controller: 'customer/top',
+      action: 'index'
+    )
+  end
+
   example 'ホスト名が対象外なら error/not_found へ' do
     expect(get: 'http://foo.example.jp').to route_to(
       controller: 'errors',
