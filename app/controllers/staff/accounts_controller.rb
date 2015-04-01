b93b62bced367521121ed7d9f5b9567e76c18a0b
kanbara <kanbara@bbtower.co.jp>
2015-04-01 10:35:11 +0900

    p257 add staff account update

--- app/controllers/staff/accounts_controller.rb
+++ app/controllers/staff/accounts_controller.rb
@@ -6,4 +6,23 @@ class Staff::AccountsController < Staff::Base
   def edit
     @staff_member = current_staff_member
   end
+
+  def update
+    @staff_member = current_staff_member
+    @staff_member.assign_attributes(staff_member_params)
+    if @staff_member.save
+      flash.notice = 'アカウントを更新しました。'
+      redirect_to :staff_account
+    else
+      render action: 'edit'
+    end
+  end
+
+  private
+  def staff_member_params
+    params.require(:staff_member).permit(
+      :email, :family_name, :given_name,
+      :family_name_kana, :given_name_kana
+    )
+  end
 end
