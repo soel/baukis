67d68711f10c8f99cce6427966fc443da639ca05
kanbara <kanbara@bbtower.co.jp>
2015-04-06 15:46:47 +0900

    p338 enshuu add family name validate

--- spec/models/staff_member_spec.rb
+++ spec/models/staff_member_spec.rb
@@ -49,6 +49,11 @@ RSpec.describe StaffMember, :type => :model do
       expect(member).not_to be_valid
     end
 
+    example '記号を含む family_name は無効' do
+      member = build(:staff_member, family_name: '試験★')
+      expect(member).not_to be_valid
+    end
+
     example '漢字を含む family_name_kana は無効' do
       member = build(:staff_member, family_name_kana: '試験')
       expect(member).not_to be_valid
