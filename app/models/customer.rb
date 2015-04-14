17592ee1b11f42ed0848be05d7ab18f149ddd3de
kanbara <kanbara@bbtower.co.jp>
2015-04-14 17:23:40 +0900

    p501 add search

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,4 +16,11 @@ class Customer < ActiveRecord::Base
     allow_blank: true
   }
 
+  before_save do
+    if birthday
+      self.birth_year = birthday.year
+      self.birth_month = birthday.month
+      self.birth_mday = birthday.mday
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414043216) do
+ActiveRecord::Schema.define(version: 20150414063243) do
 
   create_table "addresses", force: true do |t|
     t.integer  "customer_id",                null: false
