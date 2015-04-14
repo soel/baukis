2ae74c27a42f60f93361c444b6a49f0820a0b18a
kanbara <kanbara@bbtower.co.jp>
2015-04-14 13:27:50 +0900

    p488 ensyuu add coffee and work phone form

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -17,6 +17,9 @@ class Staff::CustomerForm
     (2 - @customer.home_address.phones.size).times do
       @customer.home_address.phones.build
     end
+    (2 - @customer.work_address.phones.size).times do
+      @customer.work_address.phones.build
+    end
   end
 
   def assign_attributes(params = {})
@@ -51,8 +54,19 @@ class Staff::CustomerForm
     else
       customer.home_address.mark_for_destruction
     end
+
     if inputs_work_address
       customer.work_address.assign_attributes(work_address_params)
+
+      phones = phone_params(:work_address).fetch(:phones)
+      customer.work_address.phones.size.times do |index|
+        attributes = phones[index.to_s]
+        if attributes && attributes[:number].present?
+          customer.work_address.phones[index].assign_attributes(attributes)
+        else
+          customer.work_address.phones[index].mark_for_destruction
+        end
+      end
     else
       customer.work_address.mark_for_destruction
     end
