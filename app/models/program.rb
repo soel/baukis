c29432b64c4968f3c1146986d861fe6884944d48
kanbara <kanbara@bbtower.co.jp>
2015-05-13 17:39:12 +0900

    p168 add program create update

--- app/models/program.rb
+++ app/models/program.rb
@@ -7,6 +7,9 @@ class Program < ActiveRecord::Base
     :application_start_minute, :application_end_date, :application_end_hour,
     :application_end_minute
 
+  before_validation :set_application_start_time
+  before_validation :set_application_end_time
+
   scope :listing, -> {
     joins('LEFT JOIN entries ON programs.id = entries.program_id')
       .select('programs.*, COUNT(entries.id) AS number_of_applicants')
@@ -14,4 +17,31 @@ class Program < ActiveRecord::Base
       .order(application_start_time: :desc)
       .includes(:registrant)
   }
+
+  private
+  def set_application_start_time
+    return if application_start_date.blank?
+    Date.parse(application_start_date)
+    if t = Time.zone.parse(application_start_date)
+      self.application_start_time = t.advance(
+        hours: application_start_hour.to_i,
+        minutes: application_start_minute.to_i
+      )
+    end
+  rescue ArgmentError
+    self.application_start_time = nil
+  end
+
+  def set_application_end_time
+    return if application_end_date.blank?
+    Date.parse(application_end_date)
+    if t = Time.zone.parse(application_end_date)
+      self.application_end_time = t.advance(
+        hours: application_end_hour.to_i,
+        minutes: application_end_minute.to_i
+      )
+    end
+  rescue ArgumentError
+    self.application_end_time = nil
+  end
 end
