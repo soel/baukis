41fd20444e18e33c68a69c8ff2ec612f368ffda0
kanbara <kanbara@bbtower.co.jp>
2015-05-20 10:46:45 +0900

    p194 演習問題1の実装

--- app/models/program.rb
+++ app/models/program.rb
@@ -28,6 +28,10 @@ class Program < ActiveRecord::Base
     only_integer: true, greater_than_or_equal_to: 1,
     less_than_or_equal_to: 1000, allow_blank: true
   }
+  validates :max_number_of_participants, numericality: {
+    only_integer: true, greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 1000, allow_blank: true
+  }
   validate do
     if min_number_of_participants && max_number_of_participants &&
         min_number_of_participants > max_number_of_participants
