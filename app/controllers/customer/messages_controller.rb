2b11c9ef1771db7b498e442be3fe8bafe264679d
kanbara <kanbara@bbtower.co.jp>
2015-05-27 11:59:09 +0900

    p278 確認画面の実装

--- app/controllers/customer/messages_controller.rb
+++ app/controllers/customer/messages_controller.rb
@@ -2,4 +2,20 @@ class Customer::MessagesController < Customer::Base
   def new
     @message = CustomerMessage.new
   end
+
+  # POST
+  def confirm
+    @message = CustomerMessage.new(customer_message_params)
+    if @message.valid?
+      render action: 'confirm'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
+  private
+  def customer_message_params
+    params.require(:customer_message).permit(:subject, :body)
+  end
 end
--- app/presenters/confirming_form_presenter.rb
+++ app/presenters/confirming_form_presenter.rb
@@ -3,7 +3,7 @@ class ConfirmingFormPresenter
 
   attr_reader :form_builder, :view_context
   delegate :label, :hidden_field, :object, to: :form_builder
-  
+
   def initialize(form_builder, view_context)
     @form_builder = form_builder
     @view_context = view_context
@@ -25,6 +25,17 @@ class ConfirmingFormPresenter
     markup(:div, class: 'input-block') do |m|
       m << decorated_label(name, label_text, options)
       m.div(object.send(name), class: 'field-value')
+      m << hidden_field(name, options)
+    end
+  end
+
+  def text_area_block(name, label_text, options = {})
+    markup(:div) do |m|
+      m << decorated_label(name,label_text, options)
+      value = object.send(name)
+      m.div(class: 'field-value') do
+        m << ERB::Util.html_escape(value).gsub(/\n/, '<br />')
+      end
       m << hidden_field(name, options)
     end
   end
