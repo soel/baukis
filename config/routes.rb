fa32dcb3078a48d5713d1a1d678041303b1a9fb4
kanbara <kanbara@bbtower.co.jp>
2015-05-29 17:05:16 +0900

    p360 タグによる絞り込みの実装

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,11 @@ Rails.application.routes.draw do
           post :confirm
         end
       end
+      resources :tag, only: [] do
+        resources :messages, only: [ :index ] do
+          get :inbound, :outbound, :deleted, on: :collection
+        end
+      end
     end
   end
 
