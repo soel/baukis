936a3e10ab458bd716ba19e0b7a7f22d4569f06a
kanbara <kanbara@bbtower.co.jp>
2015-05-29 15:00:58 +0900

    p331 回答の確認画面の実装

--- app/controllers/staff/replies_controller.rb
+++ app/controllers/staff/replies_controller.rb
@@ -5,8 +5,23 @@ class Staff::RepliesController < Staff::Base
     @reply = StaffMessage.new
   end
 
+  #POST
+  def confirm
+    @reply = StaffMessage.new(staff_message_params)
+    if @reply.valid?
+      render action: 'confirm'
+    else
+      flash.now.alert = '入力に誤りがあります。'
+      render action: 'new'
+    end
+  end
+
   private
   def prepare_message
     @message = CustomerMessage.find(params[:message_id])
+  end
+
+  def staff_message_params
+    params.require(:staff_message).permit(:subject, :body)
   end
 end
